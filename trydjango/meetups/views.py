6b6ad108ceb7e85ebabfe4a2e6ac682d59c88c10
zain <zainmujahid01@gmail.com>
2021-08-10 13:42:57 +0500

    Django Tutorial Files before Querrying Data

---
+++ trydjango/meetups/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+	#return HttpResponse('Hello World!')
+	meetups = [
+		{
+			'title': 'A First Meetup',
+			'location': 'New York', 
+			'slug': 'a-first-meetup'
+		},
+		{
+			'title': 'A Second Meetup', 
+			'location': 'Paris', 
+			'slug': 'a-second-meetup'
+		}
+	]
+	return render(request, 'meetups/index.html', {
+		'show_meetups' : True,
+		'meetups': meetups
+		})
+
+def meetup_details(request, meetup_slug):
+	selected_meetup = {
+	'title': 'A First Meetup', 
+	'description': 'This is the first meetup!'
+	}
+	return render(request, 'meetups/meetup-details.html', {
+		'meetup_title': selected_meetup['title'],
+		'meetup_description': selected_meetup['description']
+		})
