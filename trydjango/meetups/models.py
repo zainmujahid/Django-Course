eaea99047119f0a7ec35c18dbfce52417e97abf3
zain <zainmujahid01@gmail.com>
2021-08-10 16:27:48 +0500

    code till 'Adding Images Upload'

--- trydjango/meetups/models.py
+++ trydjango/meetups/models.py
@@ -6,3 +6,4 @@ class Meetup(models.Model):
 	title = models.CharField(max_length=200)
 	slug = models.SlugField(unique=True)
 	description = models.TextField()
+	image = models.ImageField(upload_to='images')
--- trydjango/meetups/views.py
+++ trydjango/meetups/views.py
@@ -1,32 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Meetup
 # Create your views here.
 
 def index(request):
 	#return HttpResponse('Hello World!')
-	meetups = [
-		{
-			'title': 'A First Meetup',
-			'location': 'New York', 
-			'slug': 'a-first-meetup'
-		},
-		{
-			'title': 'A Second Meetup', 
-			'location': 'Paris', 
-			'slug': 'a-second-meetup'
-		}
-	]
+	meetups = Meetup.objects.all()
 	return render(request, 'meetups/index.html', {
-		'show_meetups' : True,
 		'meetups': meetups
 		})
 
 def meetup_details(request, meetup_slug):
-	selected_meetup = {
-	'title': 'A First Meetup', 
-	'description': 'This is the first meetup!'
-	}
-	return render(request, 'meetups/meetup-details.html', {
-		'meetup_title': selected_meetup['title'],
-		'meetup_description': selected_meetup['description']
-		})
+	try:
+		selected_meetup = Meetup.objects.get(slug=meetup_slug)
+		return render(request, 'meetups/meetup-details.html', {
+			'meetup_found': True,
+			'meetup_title': selected_meetup.title,
+			'meetup_description': selected_meetup.description
+			})
+	except Exception as exc:
+		return render(request, 'meetups/meetup-details.html', {
+			'meetup_found': False
+			})
