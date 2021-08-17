6c1be13ea11b51f80849d57c8e2f6bf8f06b571a
zain <zainmujahid01@gmail.com>
2021-08-17 12:43:41 +0500

    code till one-to-many relations

--- trydjango/meetups/admin.py
+++ trydjango/meetups/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Meetup
+from .models import *
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
 	list_display = ('title', 'slug')
-	list_filter = ('title',)
+	list_filter = ('location',)
 	prepopulated_fields = {'slug': ('title',)}	
 
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
